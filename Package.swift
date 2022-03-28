0a9fedd978d2742749aab7de95e74fc6510ca4e7
Yasir Anis <yasir.anees@globizserve.com>
2022-03-28 15:45:56 +0500

    modified package with path

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "swipbox-sdk-ios",
     platforms: [
         .iOS(.v12)
-        
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -23,14 +22,17 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swipbox-sdk-ios"
-        ),
+            name: "swipbox-sdk-ios",
+            dependencies: [],
+            path: "Sources"),
         .binaryTarget(
                    name: "SwipboxSDK",
-                   path: "Sources/swipbox-sdk-ios/SwipboxSDK.xcframework"
+                   path: "./Sources/swipbox-sdk-ios/SwipboxSDK.xcframework"
         ),
         .testTarget(
             name: "swipbox-sdk-iosTests",
-            dependencies: ["swipbox-sdk-ios"]),
+            dependencies: ["swipbox-sdk-ios"],
+            path: "Tests"
+        ),
     ]
 )
